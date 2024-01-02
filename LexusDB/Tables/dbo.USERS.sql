c550d250d7bf8dbed72ef614d898815e306d05f0
Jhon Lloyd F. Omblero <113282952+joxyle-jhon04@users.noreply.github.com>
2024-01-03 00:04:17 +0800

    Added Users/Admin Table

---
+++ LexusDB/Tables/dbo.USERS.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[USERS] (
+    [Id]       INT          IDENTITY (1, 1) NOT NULL,
+    [Username] VARCHAR (50) NOT NULL,
+    [Password] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
