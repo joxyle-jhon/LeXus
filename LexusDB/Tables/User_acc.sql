12eeb4e41ff87f5006df115d99aafc78fe45aae7
Jhon Lloyd F. Omblero <113282952+joxyle-jhon04@users.noreply.github.com>
2024-01-02 22:12:57 +0800

    Added Databases

---
+++ LexusDB/Tables/User_acc.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User_Acc] (
+    [ID]       INT          IDENTITY (1, 1) NOT NULL,
+    [Username] VARCHAR (15) NOT NULL,
+    [Password] VARCHAR (20) NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([ID] ASC),
+    UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
