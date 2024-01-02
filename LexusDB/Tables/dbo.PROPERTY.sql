0f7a45e8c5f3cadbee9dda52857ae3606712d82f
Jhon Lloyd F. Omblero <113282952+joxyle-jhon04@users.noreply.github.com>
2024-01-03 00:03:31 +0800

    Added Property Table

---
+++ LexusDB/Tables/dbo.PROPERTY.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[PROPERTY] (
+    [PropertyID]      INT           IDENTITY (1, 1) NOT NULL,
+    [RoomNumber]      INT           NULL,
+    [PropertyName]    VARCHAR (50)  NULL,
+    [PropertyAddress] VARCHAR (100) NULL,
+    [WBillingNumber]  VARCHAR (50)  NULL,
+    [EBillingNumber]  VARCHAR (50)  NULL,
+    [OccupancyStatus] VARCHAR (50)  NULL,
+    [LandlordID]      INT           NULL,
+    [LeaseID]         INT           NULL,
+    [AgreementID]     INT           NULL,
+    PRIMARY KEY CLUSTERED ([PropertyID] ASC),
+    FOREIGN KEY ([LandlordID]) REFERENCES [dbo].[LANDLORD] ([LandlordID]),
+    FOREIGN KEY ([LeaseID]) REFERENCES [dbo].[USERINFO] ([LeaseID]),
+    FOREIGN KEY ([AgreementID]) REFERENCES [dbo].[AGREEMENT] ([AgreementID])
+);
+
